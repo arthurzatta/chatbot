9cdcfb27c990c76db1998cbc8d3e8fd570dcf7cb
arthur.zatta <arthur.zatta@gmail.com>
2022-05-17 22:17:28 -0300

    feat(check_invalid_chars): create a regex matcher function

--- src/lexicalAnalyser.py
+++ src/lexicalAnalyser.py
@@ -1,24 +1,28 @@
+from typing import Union
 from pathlib import Path
-
+import re as Regex
 
 class LexicalAnalyser:
     def __init__(self):
         self.stopwords = []
+        self.stream_reader()
 
     def stream_reader(self):
         try:
             stopwords_file = Path('./assets/out_stopwords.txt')
             if stopwords_file.exists():
                 with stopwords_file.open() as file:
-                  for read_word in file:
-                    strip_word = read_word.strip()
-                    self.stopwords.append(strip_word)
+                    for read_word in file:
+                        strip_word = read_word.strip()
+                        self.stopwords.append(strip_word)
         except Exception:
             print('File out_stoprwords.txt not found')
 
     def get_stopwords(self):
-      return self.stopwords
+        return self.stopwords
 
-if __name__ == '__main__':
-    analyser = LexicalAnalyser()
-    analyser.stream_reader()
+    def check_invalid_chars(self, input_string: str):
+        regex = Regex.compile(
+            '''^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?´]+(\s[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?´]+)*''')
+        return regex.match(input_string)
+
